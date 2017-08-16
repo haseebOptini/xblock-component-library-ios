991cbdd2201c3b62d33f193204d24968107cab0f
shafqat-muneer <shafqat.muneer@arbisoft.com>
2017-08-16 20:36:29 +0500

    Initial Commit

---
+++ MRQClient/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MRQClient
+//
+//  Created by Shafqat Muneer on 8/16/17.
+//  Copyright © 2017 Salman Jamil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
