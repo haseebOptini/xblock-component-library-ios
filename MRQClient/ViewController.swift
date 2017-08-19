db7e8039aacfbc33b45b0f159e3789324774cc68
shafqat-muneer <shafqat.muneer@arbisoft.com>
2017-08-19 16:44:08 +0500

    Test Changes.

--- MRQClient/ViewController.swift
+++ MRQClient/ViewController.swift
@@ -7,12 +7,35 @@
 //
 
 import UIKit
+import MckinseyXBlocks
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+//        let detailController = CourseCatalogDetailViewController(environment: environment, courseID: courseID)
+//        fromController.navigationController?.pushViewController(detailController, animated: true)
+
+        
+        let bundle = Bundle(identifier: "com.arbisoft.MckinseyXBlocks")
+        let testViewController = MyViewController(nibName: "MyViewController", bundle: bundle)
+        addChildViewController(testViewController)
+        view.addSubview(testViewController.view)
+        testViewController.view.translatesAutoresizingMaskIntoConstraints = false
+        testViewController.view.translatesAutoresizingMaskIntoConstraints = false
+//        testViewController.view.heightAnchor.constraint(equalTo: view.heightAnchor).isActive = true
+//        testViewController.view.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        testViewController.view.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        testViewController.view.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        
+        testViewController.view.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        testViewController.view.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+
+
+        
+
     }
 
     override func didReceiveMemoryWarning() {
---
+++ MckinseyXBlocks/MyViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MyViewController.swift
+//  MckinseyXBlocks
+//
+//  Created by Shafqat Muneer on 8/18/17.
+//  Copyright © 2017 Salman Jamil. All rights reserved.
+//
+
+import UIKit
+
+public class MyViewController: UIViewController {
+
+    override public func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override public func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
